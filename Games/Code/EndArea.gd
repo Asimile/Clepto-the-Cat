16089ab0e12f8edaba269c1b7c6b0f29695ecdf6
Asimile <AsimNRaja@gmail.com>
2024-04-22 00:55:55 -0400

    Add hatch close sound
    Added a sound for the hatch closing.
    Tried to implement it in a way where it plays the sound on an exact frame of the animation, but that was too weird. Internet wasn't helpful.
    Instead, I just used another timer to play it at the right time. Worked well.
    
    Also tweaked clepto walk decibel to be louder, and lowered the camera on level 4 so you can see the robots you're jumping on.

--- Games/Code/EndArea.gd
+++ Games/Code/EndArea.gd
@@ -10,21 +10,28 @@ rather than EndArea being responsible to tell it
 #@export var NEXT_LEVEL: String = "Level2"
 
 @onready var ANIM_SPRITE = $AnimatedSprite2D
-@onready var TIMER = $EndTimer
+@onready var END_TIMER = $EndTimer
+@onready var SOUND_TIMER = $SoundTimer
+@onready var HATCH_AUDIO = $HatchAudio
 
 #func start_timer():
 	## Wait for 2 seconds
 	#await(get_tree().create_timer(2.0))
-	
+
 func wait(seconds: float) -> void:
 	await get_tree().create_timer(seconds).timeout
 	
 func _on_body_entered(body):
 	body.set_control(false)
 	ANIM_SPRITE.play("close")
-	TIMER.start()
+	SOUND_TIMER.start()
+	END_TIMER.start()
 	#get_parent().get_parent()._update_scene(NEXT_LEVEL)
 
 
 func _on_end_timer_timeout():
 	get_parent().get_parent()._update_scene()
+
+
+func _on_sound_timer_timeout():
+	HATCH_AUDIO.play()
